9b572a4dd2465c0920de70b9b6a084509b6541ad
Progmech <andrei.astakhov@hotmail.com>
2022-06-27 21:18:26 +0300

    Initial Commit

---
+++ TripSheets/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TripSheets.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
